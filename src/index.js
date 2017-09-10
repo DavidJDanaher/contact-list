f89850e74bc76cb3d3f59641a20f1b1aea1684dd
David Danaher <daviddanaher@Davids-MacBook-Pro.local>
2017-09-10 12:06:46 -0500

    Webpack guide: Asset managemnt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 // import angular from 'angular';
+import './styles/style.css'
 
 function component() {
 		var element = document.createElement('div');
 
-		element.innerHTML = 'Test';
+		element.innerHTML = 'NPM change';
+		element.classList.add('home');
 
 		return element;
 }
