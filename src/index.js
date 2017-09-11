5c82448e1e7eb0e7e37d92e0da870e8776034fbc
David Danaher <daviddanaher@Davids-MacBook-Pro.local>
2017-09-10 20:38:39 -0500

    Webpack guide - Output management, incomplete

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 // import angular from 'angular';
-import './styles/style.css'
+import printMe from './print.js';
 
 function component() {
 		var element = document.createElement('div');
+		var button = document.createElement('button');
 
 		element.innerHTML = 'NPM change';
 		element.classList.add('home');
+		button.innerHTML = 'Click';
+		button.onclick = printMe;
+		element.appendChild(button);
 
 		return element;
 }
