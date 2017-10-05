201987d1f466fd7537b140be1bc5578b0577cce5
David Danaher <ddanaher@gmail.com>
2017-10-05 08:36:38 -0500

    Ui-kit: incorporated into application and included some examples for fun

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
+import uiKit from 'uikit';
+import kitIcons from 'uikit/dist/js/uikit-icons';
+
 import otherView from './features/another-view/another-view.module.js';
 import myComponent from './components/my-component/my-component.module.js';
-import myCtrl from './features/home.controller.js';
+import HomeCtrl from './features/home.controller.js';
+
 require('./resources/styles/app.style.scss');
+uiKit.use(kitIcons);
 
 export default angular.module('myAppName', [uiRouter, 'myComponent', 'AnotherViewModule'])
     .config(confgure)
-    .controller('myCtrl', myCtrl);
+    .controller('HomeCtrl', HomeCtrl);
 
 function confgure($stateProvider) {
     $stateProvider.state('Home', {
         url: '',
         template: require('./features/home.html'),
-        controller: 'myCtrl',
+        controller: 'HomeCtrl',
         controllerAs: 'homeVm'
     }).state('Feature', {
         url: '/otherFeature',
